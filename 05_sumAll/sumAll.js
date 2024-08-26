6811980e07ca51804c3fa93553410a2ce127b57c
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-08-26 20:26:04 +0100

    Pass all tests exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let sum;
 
-};
+    if((a<0) || (b<0) || (Number.isInteger(a) !== true) || (Number.isInteger(b) !== true)){
+        sum = "ERROR";
+    }
+    else{
+        sum = 0;
+        if(a < b){
+            for(a; a<=b; a++){
+                sum += a;
+            }
+        }
+        else if(a > b){
+            for(b; b<=a; b++){
+                sum += b;
+            }
+        }
+        else{
+            sum = a;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
