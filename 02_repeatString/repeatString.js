35a5847b3eda1153ccc2c539e8582cb022980e47
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-08-26 19:06:25 +0100

    Pass all conditions exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString =string;
+    let i = 0;
+   
+   if (num === 0){
+    finalString = '';
+   }
+   else if(num < 0){
+    finalString = "ERROR";
+   }
+   else if(string == ''){
+    finalString = '';
+   }
+   else{
+    while(i < num -1){
+        finalString += string;
+        i++;
+        }
+    }   
 
+    return finalString;
 };
 
 // Do not edit below this line
