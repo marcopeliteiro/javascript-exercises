d3fc99396a02058af7d2ab4fd6b86c202f30b0c3
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-09-24 10:11:56 +0100

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let split = str.replace(/[^A-Za-z0-9\s]/g,"").toLowerCase().split("");
+    let reverse = split.toReversed();
+    let join = reverse.join("");
+    let strJoin = split.join("");
+    
+    let joinedOne = join.split(" ").join("");
+    let joinedTwo = strJoin.split(" ").join("");
+
+    if(joinedOne === joinedTwo){
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
