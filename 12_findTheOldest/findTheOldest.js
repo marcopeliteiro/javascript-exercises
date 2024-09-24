a98aa8a71c3eec9ec70de3ecafe1f7c4bc430711
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-09-24 12:08:06 +0100

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    let sorti = arr.sort((a,b)=>{
+        if(a.yearOfDeath == undefined ){
+          a.yearOfDeath = new Date().getFullYear();
+        }
+        if(b.yearOfDeath == undefined){
+          b.yearOfDeath = new Date().getFullYear();  
+        }
+      let first = a.yearOfDeath - a.yearOfBirth;
+      let second = b.yearOfDeath - b.yearOfBirth;
+  
+    if(first<second){
+      return 1;
+    }
+    if(first===second){
+      return 0;
+    }
+    else{
+      return -1;
+    }
+  
+    })
+    return sorti[0];
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
