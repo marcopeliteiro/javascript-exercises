4482a24afb2fa483c970f49a8ad1115e1b124c9a
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-09-24 10:39:52 +0100

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let seq =[1,1];
+    let i=2;
+    let b = Number(a);
+    if(b===0){
+      return 0;
+    }
+    if(b<0){
+      return 'OOPS';
+    }
+    else{
+    for(i; i<b;i++){
+        seq[i] = seq[i-1] + seq[i-2];
+    }
+    }
+    return seq[b-1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
